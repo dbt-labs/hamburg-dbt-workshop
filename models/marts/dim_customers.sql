9cbe54653a3ca0a2de5766c2495249ff3380c96b
hoangphong nguyen <hoangphong.nguyen+1779175691@dbtlabsworkshop.com>
2026-05-19 09:01:48 +0000

    add jaffle shop sources

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -2,8 +2,6 @@
     config(
         materialized='table'
         )
-
-
 }}
 
 
@@ -14,7 +12,7 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ source('jaffle_shop', 'customers') }}
 
 ),
 
@@ -26,7 +24,7 @@ orders as (
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+     from {{ source('jaffle_shop', 'orders') }}
 
 ),
 
