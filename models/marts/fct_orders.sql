e1641e84c1fedd8d778f60e38fff2ddc663a1566
dennis wilk <dennis.wilk+1779175131@dbtlabsworkshop.com>
2026-05-19 14:18:09 +0000

    twes

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -6,10 +6,16 @@ payments as (
     select * from {{ ref('stg_stripe__payment') }}
 ),
 
+{% set payment_statuses = ["success","fail"] %}
+ 
 order_payments as (
     select
         order_id,
-        sum(case when status = 'success' then amount_usd end) as amount_usd
+ 
+        --sum(case when status = 'success' then amount_usd end) as amount_usd
+        {% for payment_status in payment_statuses %}
+        sum(case when status = '{{payment_status}}' then amount_usd end) as {{payment_status}}_amount_usd,
+        {% endfor %}
 
     from payments
     group by 1
@@ -21,8 +27,10 @@ final as (
         orders.order_id,
         orders.customer_id,
         orders.order_date,
-        coalesce(order_payments.amount_usd, 0) as amount_usd
-
+        --coalesce(order_payments.amount_usd, 0) as amount_usd
+        {% for payment_status in payment_statuses %}
+        coalesce(order_payments.{{payment_status}}_amount_usd, 0) as {{payment_status}}_amount_usd,
+        {% endfor %}
     from orders
     left join order_payments using (order_id)
 )
